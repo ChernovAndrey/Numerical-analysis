f7a9a823d44498361ee4e570c8ea6c9674c0b8f4
ChernovAndrey <chernov.andrey.98@mail.ru>
2018-05-17 20:26:01 +0300

    add write in file

--- Lab9/main.cpp
+++ Lab9/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 //
 //void writingData(vector<double> v, string s){
-//    const string pathToFile = "/home/andrey/CLionProjects/NumericalMethods/LAB9/files/Ui"+s+".txt";
+//    const string pathToFile = "/home/andrey/CLionProjects/NumericalMethods/Lab9/files/Ui"+s+".txt";
 //    ofstream file(pathToFile, ios_base::out | ios_base::trunc);
 //    for(int i=0;i<v.size();i++){
 //        file<< v[i]<<" ";
@@ -18,8 +18,8 @@ using namespace std;
 
 
 
-void writingData(vector<vector<double>> U){
-    const string pathToFile = "/home/andrey/CLionProjects/NumericalMethods/LAB9/files/Ui.txt";
+void writingData(vector<vector<vector<double>>> U){
+    const string pathToFile = "/home/andrey/CLionProjects/NumericalMethods/Lab9/files/U.txt";
     ofstream file(pathToFile, ios_base::out | ios_base::trunc);
 
    // file << "{";
@@ -27,33 +27,31 @@ void writingData(vector<vector<double>> U){
         auto v = U[i];
      //   file << "{ ";
         for (int j = 0; j < v.size(); j++) {
-            file << v[j]<<" ";
-            if (j!=v.size()-1){
-               // file << " ";
+            for (int k = 0; k < v[0].size(); ++k) {
+                file << v[j][k] << " ";
             }
         }
-       // file << "} ";
     }
-   // file << "}"<<endl;
     file.close();
 
-    const string pathToConfig = "/home/andrey/CLionProjects/NumericalMethods/LAB9/files/config.txt";
+    const string pathToConfig = "/home/andrey/CLionProjects/NumericalMethods/Lab9/files/config.txt";
     ofstream config(pathToConfig, ios_base::out | ios_base::trunc);
     config<<U.size()<<endl;
     config<<U[0].size()<<endl;
+    config<<U[0][0].size()<<endl;
     config.close();
 }
 
 //лямда равна единице
 vector<vector<double>> getAccuracy(double h1, double h2, double x1b, double x1f, double x2b, double x2f, double tf){ //для первого уравнения
-    //TODO: размерность на один не совпадает
     auto getSolution = [](double t, double x1,double x2){
         return 1.0;
     };
+    auto eps=1e-7;
     vector<vector<double>> U;
-    for (double i = x1b; i <= x1f; i+=h1) {
+    for (double i = x1b; i <= x1f+eps; i+=h1) {
         vector<double> Ui;
-        for (double j = x2b; j <= x2f; j+=h2) {
+        for (double j = x2b; j <= x2f+eps; j+=h2) {
             Ui.push_back( getSolution(tf,i,j) );
         }
         U.push_back(Ui);
@@ -75,17 +73,17 @@ vector<double> getAccuracy2(double h,  double x0, double xf,  double tf){
 
 int main() {
 
-    double q =2.0;
+    double q =4.0;
     double h1=0.2/q;
     double h2=0.2/q;
 //    double tau=0.0002/(q*q);  //0.062e-1 -граница сх-ти явного метода  для Ex1
-    double tau=0.2/(q);  //0.062e-1 -граница сх-ти явного метода  для Ex1
+    double tau=0.2/(4);  //0.062e-1 -граница сх-ти явного метода  для Ex1
     double x1b=0.0;
     double x2b=0.0;
     double x1f=1.0;
     double x2f=1.0;
     double t0=0.0;
-    double tf=2.0;
+    double tf=10.0;
 
     auto * solve = new Solve(h1,h2,tau,x1b,x1f,x2b,x2f,t0,tf);
     auto U = solve->calculate();
@@ -93,7 +91,7 @@ int main() {
     delete solve;
     printMatrix(U[U.size()-1]);
     cout<<"size="<<U.size()<<'\t'<<U[0].size()<<endl;
-    writingData(U[U.size()-1]);
+    writingData(U);
 //    printMatrix(U[0]);
 
 //    printMatrix(U[1]);
@@ -119,7 +117,8 @@ int main() {
 //    }
     auto UAccur =  getAccuracy(h1,h2,x1b,x1f,x2b,x2f,tf);
     auto ULast = U[U.size()-1] ;
-    cout<<"size"<<U[U.size()-1].size()<<" accur "<<UAccur.size();
+    cout<<"size"<<U[U.size()-1].size()<<" accur "<<UAccur.size()<<endl;
+    cout<<"size"<<U[U.size()-1][0].size()<<" accur "<<UAccur[0].size();
     cout<<"Resid:"<<getNormMatrix3(ULast)-getNormMatrix3(UAccur);
     return 0;
 }
